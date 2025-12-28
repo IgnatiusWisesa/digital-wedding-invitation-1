b249f0cdb6b502366d0a589404a2ba0d18a734b7
Ignatius Wisesa <wisesa.dwi@gmail.com>
2025-12-29 01:51:57 +1100

    Add ErrorBoundary to catch and display React errors

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,43 +5,46 @@ import App from './App'
 import { AdminLogin } from './pages/admin/Login'
 import { AdminDashboard } from './pages/admin/Dashboard'
 import { ProtectedRoute } from './components/admin/ProtectedRoute'
+import { ErrorBoundary } from './components/ErrorBoundary'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <Routes>
-            {/* Guest quota routes */}
-            <Route path="/" element={<App />} />
-            <Route path="/two" element={<App />} />
-            <Route path="/three" element={<App />} />
-            <Route path="/four" element={<App />} />
-            <Route path="/five" element={<App />} />
-            <Route path="/six" element={<App />} />
-            <Route path="/seven" element={<App />} />
-            <Route path="/eight" element={<App />} />
-            <Route path="/nine" element={<App />} />
-            <Route path="/ten" element={<App />} />
-            <Route path="/eleven" element={<App />} />
-            <Route path="/twelve" element={<App />} />
-            <Route path="/thirteen" element={<App />} />
-            <Route path="/fourteen" element={<App />} />
-            <Route path="/fifteen" element={<App />} />
-            <Route path="/sixteen" element={<App />} />
-            <Route path="/seventeen" element={<App />} />
-            <Route path="/eighteen" element={<App />} />
-            <Route path="/nineteen" element={<App />} />
-            <Route path="/twenty" element={<App />} />
+    <ErrorBoundary>
+        <BrowserRouter>
+            <Routes>
+                {/* Guest quota routes */}
+                <Route path="/" element={<App />} />
+                <Route path="/two" element={<App />} />
+                <Route path="/three" element={<App />} />
+                <Route path="/four" element={<App />} />
+                <Route path="/five" element={<App />} />
+                <Route path="/six" element={<App />} />
+                <Route path="/seven" element={<App />} />
+                <Route path="/eight" element={<App />} />
+                <Route path="/nine" element={<App />} />
+                <Route path="/ten" element={<App />} />
+                <Route path="/eleven" element={<App />} />
+                <Route path="/twelve" element={<App />} />
+                <Route path="/thirteen" element={<App />} />
+                <Route path="/fourteen" element={<App />} />
+                <Route path="/fifteen" element={<App />} />
+                <Route path="/sixteen" element={<App />} />
+                <Route path="/seventeen" element={<App />} />
+                <Route path="/eighteen" element={<App />} />
+                <Route path="/nineteen" element={<App />} />
+                <Route path="/twenty" element={<App />} />
 
-            {/* Admin routes */}
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route
-                path="/admin/dashboard"
-                element={
-                    <ProtectedRoute>
-                        <AdminDashboard />
-                    </ProtectedRoute>
-                }
-            />
-        </Routes>
-    </BrowserRouter>,
+                {/* Admin routes */}
+                <Route path="/admin/login" element={<AdminLogin />} />
+                <Route
+                    path="/admin/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <AdminDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    </ErrorBoundary>
 )
